84914588265aa1fa8cb43f6c08fc623a7ed7164a
ShawLevin <shawlevin@gmail.com>
2015-02-21 14:42:24 -0500

    initial
    no need for mvc

---
+++ pathfinder/Scripts/questions.js
@@ -0,0 +1,51 @@
+﻿function fetchData(tags, success, error) {
+    $.ajax({
+        url: 'https://localwiki.org/api/v4/pages/?region=320&tags=' + tags,
+        data: {},
+        type: 'GET',
+        success: success,
+        error: error
+    });
+}
+
+function updateResultsList(results) {
+
+    if (results.count == 0) {
+        $('.search-results').html('No results found.');
+        return;
+    }
+
+    $('.search-results').html('<ol></ol>');
+    for (var i = 0; i < results.count; i++) {
+        var result = results.results[i];
+
+        $('.search-results ol').append('<li><a href="' + 'https://localwiki.org/philadelphia/' + result['name'] + '"  target=_blank>' + result['name'] + '</a></li>');
+
+    }
+}
+
+function buildTopics() {
+    var topics = '';
+
+    $(".questionRadio").each(function (index) {
+        if (this.checked == true) {
+            topics = topics + this.value + ',';
+        }
+    });
+
+    $(".questionCheck").each(function (index) {
+        if (this.checked == true) {
+            topics = topics + this.value + ',';
+        }
+    });
+
+    return topics.substr(0, topics.length - 1);
+}
+
+function buildQuestions()
+{
+    //<h3>Which topics below interest you?</h3>
+    //<label><input type="checkbox" name="question1a1" class="questionCheck" value="students" />Students</label>
+    var top = ''
+    $(".questions").html("<b>hello world</b>");
+}
